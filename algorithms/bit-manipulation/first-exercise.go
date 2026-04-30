afb51a49078e66ef89685c5f1fa6a82da8b9e88a
bugnon ezequiel <bugnonezequiel@gmail.com>
2026-04-29 22:13:34 -0300

    feat: delete bug in bynaryRepresentation

--- algorithms/bit-manipulation/first-exercise.go
+++ algorithms/bit-manipulation/first-exercise.go
@@ -22,9 +22,9 @@ func BinaryRepresentation(n int) string {
 	for i := 32; i >= 0; i-- {
 		if (n & (1 << i)) != 0 {
 			result += "1"
+		} else {
+			result += "0"
 		}
-
-		result += "0"
 	}
 	return result
 }
