b7432abe29999189fb4a72ebc954ef43c5566632
bugnon ezequiel <bugnonezequiel@gmail.com>
2026-05-11 23:53:02 -0300

    feat: first intuition for factory pattern

--- patters/creational/factory/cars.go
+++ patters/creational/factory/cars.go
@@ -1,1 +1,26 @@
 package factory
+
+type ICar interface {
+	setTypeMotor()
+	isElectric() bool
+	color()
+}
+
+type Car struct {
+	name  string
+	marca string
+}
+
+func NewCar(name, marca string) *Car {
+	return &Car{
+		name:  name,
+		marca: marca,
+	}
+}
+
+func (c *Car) setTypeMotor() {}
+func (c *Car) isElectric() bool {
+	return true
+}
+func (c *Car) color() {}
+func (c *Car) getProduct(product string) (interface{}, error)
--- patters/creational/factory/factory.go
+++ patters/creational/factory/factory.go
@@ -3,3 +3,11 @@ package factory
 type IFactory interface {
 	getProduct(product string) (interface{}, error)
 }
+
+func Factory(product, witch string) interface{} {
+	if product == "car" {
+		return NewCar("", "")
+	}
+
+	return nil
+}
